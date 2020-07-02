e65f1bcc64011bd2fdead94c3853930694a6915f
Boger <2366557408@qq.com>
2020-07-02 13:22:17 +0800

    菜式

--- lib/shipu.dart
+++ lib/shipu.dart
@@ -87,10 +87,115 @@ class Caiyao extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+    TextEditingController textEditingController =new TextEditingController();
     return Scaffold(
+      appBar:AppBar(
+        backgroundColor: Colors.white,
+        automaticallyImplyLeading: false,
+        title: TextField(
+          onTap: (){
+            Navigator.pushNamed(context, "/sousuo");
+          },
+          controller: textEditingController,
+          autofocus: false,
+          decoration: InputDecoration(
+            contentPadding: EdgeInsets.all(12.0),
+            hintText: "搜索...",
+            border:
+            OutlineInputBorder(borderRadius: BorderRadius.circular(15.0)),
+
+          ),
+        ),
+
+      ),
       body:ListView(
         children: <Widget>[
+          SizedBox(height: 10,),
+          new Column(
+            children:<Widget>[
+              Container(child:Image.asset('assets/shipu/caishi/breakfast.png',width: 400.0,fit:BoxFit.fill)),
+              new Text('早  餐',textAlign: TextAlign.left),
+              SizedBox(height: 5,),
+              Container(child:Image.asset('assets/shipu/caishi/lunch.png',width: 400.0,fit:BoxFit.fill)),
+              new Text('午  餐',textAlign: TextAlign.left),
+              SizedBox(height: 5,),
+              Container(child:Image.asset('assets/shipu/caishi/dinner.png',width: 400.0,fit:BoxFit.fill)),
+              new Text('晚  餐',textAlign: TextAlign.left,),
+              SizedBox(height: 5,),
+            ]
+          ),
 
+          new Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Column(
+                  children: <Widget>[
+                    Container(
+                      child: Image.asset( 'assets/shipu/caishi/dessert.png'),
+                      height: 100.0,
+                    ),
+                    Text('面包甜点')
+                  ]
+              ),
+
+              Column(
+                  children: <Widget>[
+                    Container(
+                      child: Image.asset( 'assets/shipu/caishi/fruit.png'),
+                      height: 100.0,
+                    ),
+                    Text('水果蔬菜')
+                  ]
+              ),
+
+              Column(
+                  children: <Widget>[
+                    Container(
+                      child: Image.asset( 'assets/shipu/caishi/vegetable.png'),
+                      height: 100.0,
+                    ),
+                    Text('素   食')
+                  ]
+              ),
+            ],
+          ),
+
+          SizedBox(height: 5,),
+
+          new Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Column(
+                  children: <Widget>[
+                    Container(
+                      child: Image.asset( 'assets/shipu/caishi/gaijiaofan.png'),
+                      height: 100.0,
+                    ),
+                    Text('米饭主食')
+                  ]
+              ),
+
+              Column(
+                  children: <Widget>[
+                    Container(
+                      child: Image.asset( 'assets/shipu/caishi/soup.png'),
+                      height: 100.0,
+                    ),
+                    Text('汤   类')
+                  ]
+              ),
+
+              Column(
+                  children: <Widget>[
+                    Container(
+                      child: Image.asset( 'assets/shipu/caishi/chaomimian.png'),
+                      height: 100.0,
+                    ),
+                    Text('面食小炒')
+                  ]
+              ),
+            ],
+          )
         ],
       ),
     );
